04b284aade6b2208939029afcbba7d0067f745bb
djangogirls <shkodaonebob@gmail.com>
2022-12-07 16:51:22 +0600

    onClick, ref, VirtualDOM

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -5,15 +5,24 @@ import Post from './Post/Post';
 const MyPosts = (props) => {
   let postsElements = props.posts.map(el => <Post message={el.title} likesCount={el.likesCount} />)
 
+  let newPostElement = React.createRef(); // VirtualDOM
+
+  let addPost = () => {
+    // let text = document.getElementById('text-post').value;
+    let text = newPostElement.current.value // current - ссылается на нативный html element
+    alert(text)
+  }
+
   return (
     <div>
       <h3>My posts</h3>
       <div>
         <div>
-          <textarea></textarea>
+          {/* <textarea id="text-post"></textarea> */}
+          <textarea ref={newPostElement}></textarea>
         </div>
         <div>
-          <button>Add post</button>
+          <button onClick={ addPost }>Add post</button>
         </div>
       </div>
       <div className={classes.posts}>
